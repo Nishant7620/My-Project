9a0b807e9cddc0bd809aeae119f68aed294b4865
Nishant7620 <nkaranjavkar72@gmail.com>
2024-02-08 12:06:35 +0530

    updated

--- Modern_Home_Furnishings/core/urls.py
+++ Modern_Home_Furnishings/core/urls.py
@@ -3,7 +3,7 @@ from core import views
 
 urlpatterns = [
     path('core/',views.test),
-    path('us/',views.contact),
+    path('contact_us/',views.contact),
     path('h/about/',views.about),
     path('h/',views.Home),
     path('s/',views.sofas),
