366407cf80fdd487a09f8783a7aee4d27df018fd
Nishant7620 <nkaranjavkar72@gmail.com>
2024-02-27 01:27:34 +0530

    updated

--- Modern_Home_Furnishings/core/models.py
+++ Modern_Home_Furnishings/core/models.py
@@ -68,4 +68,25 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+class Order(models.Model):
+
+    STATUS_CHOICES = [
+        ('PENDING','Pending'),
+        ('PROCESSING','Processing'),
+        ('COMPLETED','Completed'),
+        ('CANCELLED','Cancelled')
+
+    ]   
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Customer = models.ForeignKey(Customer,on_delete=models.CASCADE)   
+    product = models.ForeignKey(Products,on_delete=models.CASCADE) 
+    quantity =models.PositiveIntegerField(default = 1)
+    order_at =models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES,default='PENDING')
+    total_price = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return str(self.id)
         
